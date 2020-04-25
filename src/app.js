e19de96adc3085ac6dcf16d7b150ed01c4576895
Amrita <amrita.pattanaik@accenture.com>
2020-04-25 17:16:54 +0530

    App.js Changes

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description : 'Water Bill', amount : 4500}));
-store.dispatch(addExpense({ description : 'Gas Bill', amount: 1200 ,createdAt : 1000}));
-store.dispatch(addExpense({ description : 'Rent Bill', amount : 109500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpense(state.expenses,state.filters);
-
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store= {store}>
         <AppRouter/>    
